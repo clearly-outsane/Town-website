e00821a53f2916095f1e441805f544f8e73a8c67
clearly-outsane <1starwarrior516@gmail.com>
2021-06-17 22:15:36 +0530

    added webgl build

---
+++ src/components/Video.js
@@ -0,0 +1,202 @@
+import React, { useEffect, useState } from "react";
+import {
+  AgoraVideoPlayer,
+  createClient,
+  createMicrophoneAndCameraTracks,
+  ClientConfig,
+  IAgoraRTCRemoteUser,
+  ICameraVideoTrack,
+  IMicrophoneAudioTrack,
+} from "agora-rtc-react";
+
+const config = {
+  mode: "rtc",
+  codec: "vp8",
+};
+
+const appId = "458915b8278f49fd84d5db3da66ca0a5"; //ENTER APP ID HERE
+const token =
+  "006458915b8278f49fd84d5db3da66ca0a5IAC8hVmt1dvJ9JNOuFuHfpLlNjlZJZMPO0dXCQgTV3gX902x7RgAAAAAEABFAsi62b/MYAEAAQDYv8xg";
+
+const Video = () => {
+  const [inCall, setInCall] = useState(false);
+  const [channelName, setChannelName] = useState("");
+  return (
+    <div>
+      <h1 className="heading">Meet scam artist Vinay</h1>
+      {inCall ? (
+        <VideoCall setInCall={setInCall} channelName={channelName} />
+      ) : (
+        <ChannelForm setInCall={setInCall} setChannelName={setChannelName} />
+      )}
+    </div>
+  );
+};
+
+// the create methods in the wrapper return a hook
+// the create method should be called outside the parent component
+// this hook can be used the get the client/stream in any component
+const useClient = createClient(config);
+const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
+
+const VideoCall = ({ setInCall, channelName }) => {
+  const [users, setUsers] = useState([]);
+  const [start, setStart] = useState(false);
+  // using the hook to get access to the client object
+  const client = useClient();
+  // ready is a state variable, which returns true when the local tracks are initialized, untill then tracks variable is null
+  const { ready, tracks } = useMicrophoneAndCameraTracks();
+
+  useEffect(() => {
+    // function to initialise the SDK
+    let init = async (name) => {
+      client.on("user-published", async (user, mediaType) => {
+        await client.subscribe(user, mediaType);
+        console.log("subscribe success");
+        if (mediaType === "video") {
+          setUsers((prevUsers) => {
+            return [...prevUsers, user];
+          });
+        }
+        if (mediaType === "audio") {
+          user.audioTrack?.play();
+        }
+      });
+
+      client.on("user-unpublished", (user, type) => {
+        console.log("unpublished", user, type);
+        if (type === "audio") {
+          user.audioTrack?.stop();
+        }
+        if (type === "video") {
+          setUsers((prevUsers) => {
+            return prevUsers.filter((User) => User.uid !== user.uid);
+          });
+        }
+      });
+
+      client.on("user-left", (user) => {
+        console.log("leaving", user);
+        setUsers((prevUsers) => {
+          return prevUsers.filter((User) => User.uid !== user.uid);
+        });
+      });
+
+      await client.join(appId, name, token, null);
+      if (tracks) await client.publish([tracks[0], tracks[1]]);
+      setStart(true);
+    };
+
+    if (ready && tracks) {
+      console.log("init ready");
+      init(channelName);
+    }
+  }, [channelName, client, ready, tracks]);
+
+  return (
+    <div className="App">
+      {ready && tracks && (
+        <Controls tracks={tracks} setStart={setStart} setInCall={setInCall} />
+      )}
+      {start && tracks && <Videos users={users} tracks={tracks} />}
+    </div>
+  );
+};
+
+const Videos = ({ users, tracks }) => {
+  return (
+    <div>
+      <div id="videos">
+        {/* AgoraVideoPlayer component takes in the video track to render the stream,
+              you can pass in other props that get passed to the rendered div */}
+        <AgoraVideoPlayer
+          style={{ height: "95%", width: "95%" }}
+          className="vid"
+          videoTrack={tracks[1]}
+        />
+        {users.length > 0 &&
+          users.map((user) => {
+            if (user.videoTrack) {
+              return (
+                <AgoraVideoPlayer
+                  style={{ height: "95%", width: "95%" }}
+                  className="vid"
+                  videoTrack={user.videoTrack}
+                  key={user.uid}
+                />
+              );
+            } else return null;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export const Controls = ({ tracks, setStart, setInCall }) => {
+  const client = useClient();
+
+  const [trackState, setTrackState] = useState({ video: true, audio: true });
+
+  const mute = async (type) => {
+    if (type === "audio") {
+      await tracks[0].setEnabled(!trackState.audio);
+      setTrackState((ps) => {
+        return { ...ps, audio: !ps.audio };
+      });
+    } else if (type === "video") {
+      await tracks[1].setEnabled(!trackState.video);
+      setTrackState((ps) => {
+        return { ...ps, video: !ps.video };
+      });
+    }
+  };
+
+  const leaveChannel = async () => {
+    await client.leave();
+    client.removeAllListeners();
+    // we close the tracks to perform cleanup
+    tracks[0].close();
+    tracks[1].close();
+    setStart(false);
+    setInCall(false);
+  };
+
+  return (
+    <div className="controls">
+      <p className={trackState.audio ? "on" : ""} onClick={() => mute("audio")}>
+        {trackState.audio ? "MuteAudio" : "UnmuteAudio"}
+      </p>
+      <p className={trackState.video ? "on" : ""} onClick={() => mute("video")}>
+        {trackState.video ? "MuteVideo" : "UnmuteVideo"}
+      </p>
+      {<p onClick={() => leaveChannel()}>Leave</p>}
+    </div>
+  );
+};
+
+const ChannelForm = ({ setInCall, setChannelName }) => {
+  return (
+    <form className="join">
+      {appId === "" && (
+        <p style={{ color: "red" }}>
+          Please enter your Agora App ID in App.tsx and refresh the page
+        </p>
+      )}
+      <input
+        type="text"
+        placeholder="Enter Channel Name"
+        onChange={(e) => setChannelName(e.target.value)}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setInCall(true);
+        }}
+      >
+        Join
+      </button>
+    </form>
+  );
+};
+
+export default Video;
